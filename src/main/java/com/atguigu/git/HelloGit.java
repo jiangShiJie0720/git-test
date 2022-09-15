26367f266d7c40a9cbb598580301364fd0b802da
jsj <752911615@qq.com>
2022-09-15 09:06:49 +0800

    hot-fix commit1.0

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
         System.out.println("Hello git3!");
+        System.out.println("Hello git4!");
+        System.out.println("modified by hot-fix");
     }
 }
